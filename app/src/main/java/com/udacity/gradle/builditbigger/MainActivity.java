776bbe5d9fc5e1bd1d6cec565f5736d5613442d3
Rofaeil Ashaiaa <RofaeilAshaiaa@gmail.com>
2017-05-06 16:23:04 +0200

    Remove the ad (and any dependencies you can) from the paid flavor

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,11 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import rofaeil.ashaiaa.idea.jokeviewlibrary.JokeTellingActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -42,7 +45,10 @@ public class MainActivity extends AppCompatActivity {
     public void tellJoke(View view) {
 
         JokesProvider jokesProvider = new JokesProvider();
-        Toast.makeText(this, jokesProvider.getJoke() , Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this, JokeTellingActivity.class);
+        intent.putExtra(getString(R.string.joke_key), jokesProvider.getJoke());
+        startActivity(intent);
+//        Toast.makeText(this, jokesProvider.getJoke() , Toast.LENGTH_SHORT).show();
     }
 
 
